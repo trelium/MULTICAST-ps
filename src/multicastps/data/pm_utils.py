d23c7320710c5027cf9216e5f1d04b484f0d336c
trelium <jacopo.mocellin@gmail.com>
2025-11-19 11:17:15 +0100

    ch/nyu addresses

--- src/multicastps/data/pm_utils.py
+++ src/multicastps/data/pm_utils.py
@@ -7,11 +7,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def download_pm_data(path, videos = False, id = ''):
+def download_pm_data(path, videos = False, id = '', site = 'ch'):
     """
     :path: path to folder where to save files. A folder named pm will be created here. If already present, it will first be deleted.  
     :videos: if True, download also video data, if false download only passive sensing and questionnaire data 
     :id: only download data for one specific participant by specifying a participant id here. if not specified and VIDEOS = True, download data for all users in coaching WITH video files (Caution: Takes very long!)
+    :ch: specify which site of data collection to get data for, either 'nyu' or 'ch'
     """
     pm_folder = os.path.join(path, 'pm')
     if os.path.exists(pm_folder):
@@ -25,17 +26,27 @@ def download_pm_data(path, videos = False, id = ''):
     # Create a new 'pm' folder
     os.makedirs(pm_folder)
 
+    url_dict = {
+        'ch': {
+            'auth_url': 'https://pmcp-multicast.bli.uzh.ch/PMCPm-PCMS/api/v00/authentication/login',
+            'int_url': 'https://pmcp-multicast.bli.uzh.ch/PMCPm-PCMS/api/v03/team/basics',
+            'data_url_base': 'https://pmcp-multicast.bli.uzh.ch/PMCP/api/v04/pcms/downloadInterventionAnalytics/'},
+        'nyu': {
+            'auth_url': 'https://pmcp-multicast-nyu.bli.uzh.ch/PMCPm-PCMS/api/v00/authentication/login',
+            'int_url': 'https://pmcp-multicast-nyu.bli.uzh.ch/PMCPm-PCMS/api/v03/team/basics',          
+            'data_url_base': 'https://pmcp-multicast-nyu.bli.uzh.ch/PMCP/api/v04/pcms/downloadInterventionAnalytics/'}
+    }
+
     #get auth info
-    auth_url = 'https://pmcp-multicast.bli.uzh.ch/PMCPm-PCMS/api/v00/authentication/login'
     auth_cont = { 'username': os.environ.get('PM_USER'), 'password': os.environ.get('PM_PW'), 'twoFactorKey': "0", 'locale': "en-GB" }
-    resp = requests.post(url = auth_url, json=auth_cont)
+    resp = requests.post(url = url_dict[site]['auth_url'], json=auth_cont)
+    print(resp)
     auth_tok  = resp.json()
     auth_tok = auth_tok['token'] 
-    int_url =  'https://pmcp-multicast.bli.uzh.ch/PMCPm-PCMS/api/v03/team/basics'
-    resp = requests.get(url=int_url, headers={'authorization' : auth_tok})
+    resp = requests.get(url=url_dict[site]['int_url'], headers={'authorization' : auth_tok})
     objectId = resp.json()
     objectId = objectId['interventionOptions'][0]['objectId']['objectId']
-    data_url = 'https://pmcp-multicast.bli.uzh.ch/PMCP/api/v04/pcms/downloadInterventionAnalytics/' + objectId
+    data_url = url_dict[site]['data_url_base'] + objectId  
 
 
     if len(id) != 0:
@@ -85,8 +96,14 @@ if __name__ == '__main__':
         action="store_true",
         default=False
         )
+    parser.add_argument(
+        "--site",
+        help="Specify which site of data collection to get data for, either nyu or ch",
+        type=str,
+        default='ch'
+    )
     
     args = parser.parse_args()
-    download_pm_data(args.path, args.videos, args.id )
+    download_pm_data(args.path, args.videos, args.id, args.site)
 
     
